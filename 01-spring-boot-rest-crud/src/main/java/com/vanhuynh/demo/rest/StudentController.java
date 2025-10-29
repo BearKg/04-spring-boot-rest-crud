3f6bd7ce2e6021ff08eb24535b329eb5fd7720ef
hahuynhvan <hahuynhhuy159@gmail.com>
2025-10-29 21:02:59 +0700

    first commit

---
+++ 01-spring-boot-rest-crud/src/main/java/com/vanhuynh/demo/rest/StudentController.java
@@ -0,0 +1,41 @@
+package com.vanhuynh.demo.rest;
+
+import com.vanhuynh.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private List<Student> theStudents;
+
+    // Load data after initiating bean
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/student" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    //define endpoint or "/students/{studentId}" - return student index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // just index from the list
+        return theStudents.get(studentId);
+    }
+}
---
+++ 02-spring-boot-rest-crud-exception-handling/src/main/java/com/vanhuynh/demo/rest/StudentController.java
@@ -0,0 +1,74 @@
+package com.vanhuynh.demo.rest;
+
+import com.vanhuynh.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private List<Student> theStudents;
+
+    // Load data after initiating bean
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/student" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    //define endpoint or "/students/{studentId}" - return student index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // just index from the list
+
+        // check the studentId again list size
+
+        if((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+
+    // Add  an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
